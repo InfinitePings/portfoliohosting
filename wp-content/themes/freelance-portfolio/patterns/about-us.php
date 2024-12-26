cddd55e04d80f7aed647673600e17a7244342b91
unknown <akshaydudhe943@gmail.com>
2024-12-26 11:32:23 +0530

    Uploading website files

---
+++ wp-content/themes/freelance-portfolio/patterns/about-us.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: Product
+ * Slug: freelance-portfolio/about-us
+ * Categories: about-us
+ * Block Types: core/template-part/about-us
+ */
+?>
+
+<!-- wp:group {"tagName":"main","className":"wp-block-cover__background","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"},"color":{"gradient":"linear-gradient(135deg,rgb(25,25,25) 77%,rgba(255,0,128,0.63) 100%)"}},"layout":{"type":"constrained","contentSize":"80%"}} -->
+<main class="wp-block-group wp-block-cover__background has-background" style="background:linear-gradient(135deg,rgb(25,25,25) 77%,rgba(255,0,128,0.63) 100%);margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:800"><!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"verticalAlignment":"center","className":"product-main"} -->
+<div class="wp-block-columns are-vertically-aligned-center product-main"><!-- wp:column {"verticalAlignment":"center","width":"60%","className":"about-text-box"} -->
+<div class="wp-block-column is-vertically-aligned-center about-text-box" style="flex-basis:60%"><!-- wp:heading {"textAlign":"center","level":6,"className":"short-heading","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"13px","textTransform":"capitalize"},"spacing":{"padding":{"top":"10px","bottom":"10px"}}},"textColor":"base","gradient":"primary-gradient","fontFamily":"nunito"} -->
+<h6 class="wp-block-heading has-text-align-center short-heading has-base-color has-primary-gradient-gradient-background has-text-color has-background has-link-color has-nunito-font-family" style="padding-top:10px;padding-bottom:10px;font-size:13px;font-style:normal;font-weight:700;text-transform:capitalize"><?php echo esc_html('Our Achievements','freelance-portfolio'); ?></h6>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"32px","fontStyle":"normal","fontWeight":"700"}},"textColor":"base","fontFamily":"nunito"} -->
+<h3 class="wp-block-heading has-base-color has-text-color has-link-color has-nunito-font-family" style="font-size:32px;font-style:normal;font-weight:700"><?php echo esc_html('Innovation, Passion, Precision, ','freelance-portfolio'); ?><br><?php echo esc_html('Excellence.','freelance-portfolio'); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"500"},"color":{"text":"#8c93a6"},"elements":{"link":{"color":{"text":"#8c93a6"}}}},"fontFamily":"nunito"} -->
+<p class="has-text-color has-link-color has-nunito-font-family" style="color:#8c93a6;font-size:16px;font-style:normal;font-weight:500"><?php echo esc_html('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non erat ','freelance-portfolio'); ?><br><?php echo esc_html('leo. Vivamus lacinia tincidunt arcu, ut egestas nulla volutpat et. Ut ','freelance-portfolio'); ?><br><?php echo esc_html('in nibh sed neque mollis tempus at et neque.','freelance-portfolio'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center","className":"counter-main"} -->
+<div class="wp-block-columns are-vertically-aligned-center counter-main"><!-- wp:column {"verticalAlignment":"center","className":"counter-box","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"blockGap":"0"},"border":{"width":"0px","style":"none","radius":"10px"},"color":{"background":"#373737"}}} -->
+<div class="wp-block-column is-vertically-aligned-center counter-box has-background" style="border-style:none;border-width:0px;border-radius:10px;background-color:#373737;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"900","fontSize":"45px"},"spacing":{"padding":{"top":"0","bottom":"10px","right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"nunito"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-top:0;padding-right:0;padding-bottom:10px;padding-left:0;font-size:45px;font-style:normal;font-weight:900">26</h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":6,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"16px","textTransform":"capitalize","fontStyle":"normal","fontWeight":"600"},"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"10px"}}},"textColor":"base","fontFamily":"nunito"} -->
+<h6 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-top:0;padding-right:0;padding-bottom:10px;padding-left:0;font-size:16px;font-style:normal;font-weight:600;text-transform:capitalize"><?php echo esc_html('Years','freelance-portfolio'); ?><br><?php echo esc_html('Experiences','freelance-portfolio'); ?></h6>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","className":"counter-box","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"},"blockGap":"0"},"color":{"background":"#373737"},"border":{"radius":"10px"}}} -->
+<div class="wp-block-column is-vertically-aligned-center counter-box has-background" style="border-radius:10px;background-color:#373737;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"900","fontSize":"45px"},"spacing":{"padding":{"top":"0","bottom":"10px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"nunito"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-top:0;padding-bottom:10px;font-size:45px;font-style:normal;font-weight:900">120</h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":6,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"16px","textTransform":"capitalize"},"spacing":{"padding":{"bottom":"10px"}}},"textColor":"base","fontFamily":"nunito"} -->
+<h6 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-bottom:10px;font-size:16px;font-style:normal;font-weight:600;text-transform:capitalize"><?php echo esc_html('Commercial ','freelance-portfolio'); ?>&amp; <?php echo esc_html('Public Space','freelance-portfolio'); ?></h6>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","className":"counter-box","style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"color":{"background":"#373737"},"border":{"radius":"10px"}}} -->
+<div class="wp-block-column is-vertically-aligned-center counter-box has-background" style="border-radius:10px;background-color:#373737;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"900","fontSize":"45px"},"spacing":{"padding":{"top":"0","bottom":"10px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontFamily":"nunito"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-top:0;padding-bottom:10px;font-size:45px;font-style:normal;font-weight:900">16</h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":6,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"16px","textTransform":"capitalize"},"spacing":{"padding":{"bottom":"10px"}}},"textColor":"base","fontFamily":"nunito"} -->
+<h6 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-nunito-font-family" style="padding-bottom:10px;font-size:16px;font-style:normal;font-weight:600;text-transform:capitalize"><?php echo esc_html('Industry Awards Won','freelance-portfolio'); ?></h6>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"40%","className":"about-image-box"} -->
+<div class="wp-block-column is-vertically-aligned-center about-image-box" style="flex-basis:40%"><!-- wp:image {"id":23,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/about-image.png" alt="" class="wp-image-23"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></main>
+<!-- /wp:group -->
---
+++ wp-content/themes/freelance-portfolio/readme.txt
@@ -0,0 +1,62 @@
+=== Freelance Portfolio ===
+Contributors: titanthemes
+Requires at least: 6.1
+Tested up to: 6.7
+Requires PHP: 7.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+The Freelance Portfolio WordPress Theme is a versatile and dynamic solution for showcasing your work in an elegant and professional manner. Designed for freelancers, artists, designers, photographers, and content creators, this theme provides an ideal platform to build a creative portfolio website. Whether you’re showcasing graphic design projects, photography collections, web designs, or branding portfolios, this theme ensures your work is presented in a visually stunning and organized layout. Its responsive portfolio design guarantees flawless display across all devices, from desktops to mobile phones. The theme includes customizable portfolio templates, allowing you to create a unique portfolio that reflects your personal style or brand. Features like portfolio galleries, interactive elements, and animated sections bring your portfolio to life, while built-in portfolio management tools simplify the organization of your projects. With its minimalist portfolio layouts and clean design, the Freelance Portfolio WordPress Theme offers a modern, user-friendly interface. Perfect for personal branding portfolios and online resume portfolios, it also integrates a blog section for sharing insights, tutorials, or updates. Freelancers can use the drag-and-drop portfolio builder to quickly customize their website without technical expertise. Whether you're an architect, makeup artist, videographer, or writer, this theme empowers you to create a compelling and professional online presence.
+
+== Changelog ==
+
+= 1.0 =
+* Released: December 9, 2024
+
+== Copyright ==
+
+Freelance Portfolio WordPress Theme, (C) 2022-2024 Titan Themes
+Freelance Portfolio is distributed under the terms of the GNU GPL.
+
+Freelance Portfolio WordPress Theme incorporates code from Twenty Twenty-Three WordPress Theme, Copyright 2022-2024 WordPress.org
+Twenty Twenty-Three WordPress Theme is distributed under the terms of the GNU GPL
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://pxhere.com/en/license
+Source: https://pxhere.com/en/photo/1327741
+
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://pxhere.com/en/license
+Source: https://pxhere.com/en/photo/672062
+
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://pxhere.com/en/license
+Source: https://pxhere.com/en/photo/548754
+
+
+Google Web Fonts ( outfit, Orbitron, Inter, Oswald, Roboto+Flex, Nunito) By Google - https://google.com
+
+* Outfit, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Outfit?query=outfit
+
+* Orbitron, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Orbitron?query=Orbitron
+
+* Inter, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Inter?query=inter
+
+* Oswald, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Oswald?query=Oswald
+
+* Roboto+Flex, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Roboto+Flex?query=+Roboto+Flex
+
+* Nunito, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Nunito?query=Nunito
